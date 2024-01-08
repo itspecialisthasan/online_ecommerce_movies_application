55d463c5c81951b222a1cdd0e1a156217d96c94a
KAM <KAM@DESKTOP-T98R18H>
2024-01-08 12:52:59 -0800

    Add project files.

---
+++ online_ecommerce_movies_application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace online_ecommerce_movies_application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
